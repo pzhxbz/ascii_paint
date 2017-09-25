c551b1c32695b8824b0b17e6df819ec110064008
xian <pzhxbz@QQ.com>
2017-09-25 20:32:31 +0800

    改成类，顺便写了生产QR的方法

--- ascii_paint.py
+++ ascii_paint.py
@@ -1,21 +1,56 @@
-import numpy as np
+# -*- coding:utf-8 -*-
+
 from skimage import io, transform
 import sys
 
 
-def convert_grey_to_ascii(grey):
-    if grey > 0.8:
-        return '  '
-    if grey > 0.6:
-        return '..'
-    if grey > 0.5:
-        return '--'
-    if grey > 0.4:
-        return 'OO'
-    if grey > 0.2:
-        return '**'
-    else:
-        return '##'
+class AsciiPaint:
+    def __init__(self, photo_path):
+        self.__img = io.imread(photo_path, as_grey=True)
+
+    def get_QRcode(self, output_size=None, format_pre='', format_fin=''):
+        if output_size is None:
+            img = self.__img
+        else:
+            img = transform.resize(self.__img, output_size)
+
+        output_format = ''
+        for i in range(output_size[0]):
+            output_format += format_pre
+            for j in range(output_size[1]):
+                output_format += (lambda x: '  ' if x > 0.8 else '██')(img[i][j])
+            output_format += format_fin + '\n'
+        return output_format
+
+    def get_ascii_paint(self, output_size=None, format_pre='', format_fin=''):
+        if output_size is None:
+            img = self.__img
+        else:
+            img = transform.resize(self.__img, output_size)
+
+        output_format = ''
+        for i in range(output_size[0]):
+            output_format += format_pre
+            for j in range(output_size[1]):
+                output_format += self._convert_grey_to_ascii(img[i][j])
+            output_format += format_fin + '\n'
+        return output_format
+
+    @staticmethod
+    def _convert_grey_to_ascii(grey):
+        if grey > 0.8:
+            return '  '
+        # return '██'
+        if grey > 0.6:
+            return '..'
+        if grey > 0.5:
+            return '--'
+        if grey > 0.4:
+            return 'OO'
+        if grey > 0.2:
+            return '**'
+        else:
+            return '##'
 
 
 def main(argv):
@@ -23,29 +58,17 @@ def main(argv):
         print '<usage> photo_name <optional>size_w size_h'
         return
     photo_name = argv[1]
-    try:
-        img = io.imread(photo_name, as_grey=True)
-    except EOFError:
-        print 'file not found'
-        return
-
-    output_size = (img.shape[0], img.shape[1])
-
+    ap = AsciiPaint(photo_name)
     if len(argv) == 4:
         try:
             output_size = (int(argv[2]), int(argv[3]))
-            img = transform.resize(img, output_size)
-        except Exception:
-            pass
-    # print output_size
-    output_format = ''
-    for i in range(output_size[0]):
-        output_format += 'puts("'
-        for j in range(output_size[1]):
-            output_format += convert_grey_to_ascii(img[i][j])
-        output_format += '");\n'
-    print output_format
+        except ValueError:
+            print 'parse size error'
+            return
+        print ap.get_ascii_paint(output_size=output_size)
+        return
+    print ap.get_ascii_paint()
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(sys.argv) 
